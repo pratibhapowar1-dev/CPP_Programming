cc0befbde18d79135dc29af8df1a7d8e8ff969c4
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-07 15:54:21 +0530

    Addition of two floats

---
+++ program8.cpp
@@ -0,0 +1,95 @@
+/*
+Algorithem
+START
+        Accept first number as no1
+        Accept second number as no2
+        if input is nigetive then convert it into positive
+        perform addition of no and no2
+        Display the addition on screen     
+STOP
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+// Required header file
+//////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : AdditionTwoNumbers        
+//  Description :   It is use to perform addition  
+//  input :         int, int  
+//  output :        int
+//  Author :        Pratibha Sagar Pawar
+//  Date :          09/10/25 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+float AdditionTwoNumbers(
+                       float iNo1,          // first input
+                       float iNo2           // second input
+                     )
+{
+    float iSum = 0;                         // to store the resulet   
+  
+    if(iNo1 < 0.0f)                          // updater
+    {
+        iNo1  = -iNo1;
+    }     
+    
+    if(iNo2<0.0f)                            // updater
+    {
+        iNo2 =- iNo2;
+    }
+ 
+    iSum = iNo1+iNo2;                     // Business Logic
+    return iSum;
+
+}
+///////////////////////////////////////////////////////////////////////////////
+// End of AdditionTwoNumbes
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//  Entery point function
+//////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    float iValue1 = 0.0f, iValue2 = 0.0f;        // to accept user input    
+
+    float fRet =0.0f;                         // to store the result
+
+    cout<<"Enter first number :\n";
+    cin>>iValue1;
+
+    cout<<"Enter second number :\n";
+    cin>>iValue2;
+    
+
+    fRet = AdditionTwoNumbers(iValue1,iValue2);  // function call
+
+    cout<<"Addition is : "<<fRet<<"\n";
+
+
+    
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//  End of entry point function
+////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Testcasess successfully handaled by the applications
+//
+//   Input 1 : 10.0        Input2 : 3.0          output : 13
+//   Input 1 : -10.0       Input2 : 3.0          output : 13
+//   Input 1 : 10.0        Input2 : -3.0         output : 13
+//   Input 1 : -10.0       Input2 : -3.0         output : 13
+//
+///////////////////////////////////////////////////////////////////////////////
