e4d585856c25982a7592469d55e02718afb0c58d
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-10 20:09:20 +0530

    Displya digits with the help of for loop

---
+++ program17.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt<= iNo; iCnt++)
+    {
+        cout<<iCnt<<"\t";
+    }
+  
+    cout<<"\n";
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequancy : \n");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+
+    return 0;
+}
