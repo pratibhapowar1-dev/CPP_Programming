da7817d45ee056f08a3784998746d84ef6288877
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-10 20:15:54 +0530

    Display reverse number

---
+++ program21.cpp
@@ -0,0 +1,26 @@
+//5 4 3 2 1
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt>= 1; iCnt--)
+    {
+        cout<<iCnt<<"\t";
+    }
+  
+    cout<<"\n";
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequancy : \n");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+
+    return 0;
+}
